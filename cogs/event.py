98a72ac81875aa62e35ed63034a28565a293ec69
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-05-28 11:25:35 +0200

    update view message

--- cogs/event.py
+++ cogs/event.py
@@ -13,7 +13,7 @@ class EventCog(commands.Cog):
     async def create_event(self, interaction: discord.Interaction, nom: str, description: str, date: str):
         result = insert_event(nom, description, date)
         if isinstance(result, str):
-            await interaction.response.send_message(f"erreur: ```python\n{result}\n```")
+            await interaction.response.send_message(f"> erreur: \n> {result}\n")
             return
 
         await interaction.response.send_message("insertion réussis!")
@@ -22,15 +22,15 @@ class EventCog(commands.Cog):
     async def see_all_events(self, interaction: discord.Interaction):
         result = get_all_events()
         if isinstance(result, str):
-            await interaction.response.send_message(f"erreur: ```python\n{result}\n```")
+            await interaction.response.send_message(f"> erreur: \n> {result}\n")
             return
 
         content = ""
         for event in result:
-            content += f"```nom: {event.name}\ndescription: {event.description}\ndate: {event.date}```"
+            content += f"> nom: {event.name}\n> description: {event.description}\n> date: {event.date}`"
 
         if not content:
-            await interaction.response.send_message("pas encore d'évènements pour le moment")
+            await interaction.response.send_message("> pas encore d'évènements pour le moment")
             return
 
         await interaction.response.send_message(content)
@@ -39,29 +39,29 @@ class EventCog(commands.Cog):
     async def edit_event(self, interaction: discord.Interaction, nom_actuel: str, nom: str = None, description: str = None, date: str = None):
         result = get_event_by_name(nom_actuel)
         if isinstance(result, str):
-            await interaction.response.send_message(f"erreur: ```python\n{result}\n```")
+            await interaction.response.send_message(f"> erreur: \n> {result}")
             return
 
         result = update_event(result.id, nom or result.name, description or result.description, date or result.date)
         if isinstance(result, str):
-            await interaction.response.send_message(f"erreur: ```python\n{result}\n```")
+            await interaction.response.send_message(f"> erreur: \n> {result}")
             return
 
-        await interaction.response.send_message("message modifié avec succès")
+        await interaction.response.send_message("> message modifié avec succès")
 
     @app_commands.command(name="supprimer_un_evenement", description="permet de supprimer un évènement")
     async def delete_event(self, interaction: discord.Interaction, nom: str):
         result = get_event_by_name(nom)
         if isinstance(result, str):
-            await interaction.response.send_message(f"erreur: ```python\n{result}\n```")
+            await interaction.response.send_message(f"> erreur: \n> {result}")
             return
 
         result = delete_event(result.id)
         if isinstance(result, str):
-            await interaction.response.send_message(f"erreur: ```python\n{result}\n```")
+            await interaction.response.send_message(f"> erreur: \n> {result}")
             return
 
-        await interaction.response.send_message("message supprimé avec succès")
+        await interaction.response.send_message("> message supprimé avec succès")
 
 
 async def setup(bot):
